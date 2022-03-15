3c3c752624273f28a4168ecb454b19d2620e8f08
belen <belenchu-2001@hotmail.com>
2022-03-15 11:31:05 +0100

    Paredes estrechas ok

--- marselotech_my_world/install/setup.bash
+++ marselotech_my_world/install/setup.bash
@@ -22,10 +22,7 @@ _colcon_prefix_chain_bash_source_script() {
 COLCON_CURRENT_PREFIX="/opt/ros/foxy"
 _colcon_prefix_chain_bash_source_script "$COLCON_CURRENT_PREFIX/local_setup.bash"
 # setting COLCON_CURRENT_PREFIX avoids determining the prefix in the sourced script
-COLCON_CURRENT_PREFIX="/home/leire/install"
-_colcon_prefix_chain_bash_source_script "$COLCON_CURRENT_PREFIX/local_setup.bash"
-# setting COLCON_CURRENT_PREFIX avoids determining the prefix in the sourced script
-COLCON_CURRENT_PREFIX="/home/leire/turtlebot3_ws/install"
+COLCON_CURRENT_PREFIX="/home/belen/turtlebot3_ws/install"
 _colcon_prefix_chain_bash_source_script "$COLCON_CURRENT_PREFIX/local_setup.bash"
 
 # source this prefix
