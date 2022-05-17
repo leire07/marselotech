7c3c4d38d71894a8595c80c7debc9ae324e546a0
belen <belenchu-2001@hotmail.com>
2022-05-17 18:30:28 +0200

    Ficheros capturar rostro y personas en real

---
+++ capture_image/capture_image/detectar_verde_real.py
@@ -0,0 +1,58 @@
+import rclpy
+import cv2
+import numpy as np
+from cv_bridge import CvBridge, CvBridgeError
+from sensor_msgs.msg import Image
+from rclpy.node import Node
+from rclpy.qos import ReliabilityPolicy, QoSProfile
+
+class Ros2OpenCVImageConverter(Node):   
+
+    def __init__(self):
+
+        super().__init__('Ros2OpenCVImageConverter')
+        
+        self.bridge_object = CvBridge()
+        self.image_sub = self.create_subscription(Image,'/image',self.camera_callback,QoSProfile(depth=10, reliability=ReliabilityPolicy.BEST_EFFORT))
+        
+    def camera_callback(self,data):
+
+        try:
+            # Seleccionamos bgr8 porque es la codificacion de OpenCV por defecto
+            cv_image = self.bridge_object.imgmsg_to_cv2(data, desired_encoding="bgr8")
+        except CvBridgeError as e:
+            print(e)
+                    
+        hsv = cv2.cvtColor(cv_image,cv2.COLOR_BGR2HSV)
+
+        lower_green = np.array([36, 25, 25])
+        upper_green = np.array([86, 255, 255])
+
+        mask = cv2.inRange(hsv, lower_green, upper_green)
+
+        res = cv2.bitwise_and(cv_image, cv_image, mask= mask)
+     
+        
+
+        cv2.imshow('Verde detectado',res)
+        
+
+        cv2.waitKey(1) #aprieta una tecla 
+
+def main(args=None):
+
+    rclpy.init(args=args)    
+    img_converter_object = Ros2OpenCVImageConverter()    
+       
+    try:
+        rclpy.spin(img_converter_object)
+    except KeyboardInterrupt:
+        img_converter_object.destroy_node()
+        print("Fin del programa!")
+
+
+    cv2.destroyAllWindows() 
+    
+
+if __name__ == '__main__':
+    main()
--- capture_image/setup.py
+++ capture_image/setup.py
@@ -23,7 +23,12 @@ setup(
             'capture_image=capture_image.capturar:main',
             'capture_person=capture_image.capturar_personas:main',
             'capture_green=capture_image.detectar_verde:main',
-            'capture_face=capture_image.capturar_caras:main'
+            'capture_face=capture_image.capturar_caras:main',
+            'capture_person_real=capture_image.capturar_personas_real:main',
+            'capture_green_real=capture_image.detectar_verde_real:main',
+            'capture_face_real=capture_image.capturar_caras_real:main',
+            'capture_image_real=capture_image.capturar_real:main'
+
         ],
     },
 )
