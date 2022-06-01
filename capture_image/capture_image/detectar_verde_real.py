d01ce9cea26480a7e84684ba8e4067af273dec2a
Leire Villarroya Martínez <lvilmar1@epsg.upv.es>
2022-06-01 16:22:01 +0200

    T058 - Arreglar script detección verde

--- capture_image/capture_image/detectar_verde_real.py
+++ capture_image/capture_image/detectar_verde_real.py
@@ -8,28 +8,27 @@ from rclpy.qos import ReliabilityPolicy, QoSProfile
 kernel = np.ones((7,7),np.uint8)
 
 class Ros2OpenCVImageConverter(Node):   
+
     """
   Clase para detectar el color verde en una persona
 
   Attributes:
     bridge_object (CvBridge): tranforma la imagen a csv
-    image_sub (subscriber): suscriptor del topic /image
+    image_sub (subscriber): suscriptor del topic /camera/image_raw
   
   Methods:
     camera_callback(): metodo para detectar la imagen y que detecte el color verde por la persona
 
 
-  """ 
-
+  """
 
     def __init__(self):
 
         super().__init__('Ros2OpenCVImageConverter')
         
         self.bridge_object = CvBridge()
-        self.image_sub = self.create_subscription(Image,'/image',self.camera_callback,QoSProfile(depth=10, reliability=ReliabilityPolicy.BEST_EFFORT))
+        self.image_sub = self.create_subscription(Image,'/camera/image',self.camera_callback,QoSProfile(depth=10, reliability=ReliabilityPolicy.BEST_EFFORT))
         
-
     def camera_callback(self,data):
 
         """ Función que se activa al detectar una imagen por el topic /camre/image_raw
@@ -39,15 +38,32 @@ class Ros2OpenCVImageConverter(Node):
         data(imagen)
 
         """
-
         try:
             # Seleccionamos bgr8 porque es la codificacion de OpenCV por defecto
             cv_image = self.bridge_object.imgmsg_to_cv2(data, desired_encoding="bgr8")
-            print("iniciar")
         except CvBridgeError as e:
             print(e)
                     
         hsv = cv2.cvtColor(cv_image,cv2.COLOR_BGR2HSV)
+        img_gray = cv2.cvtColor(cv_image, cv2.COLOR_BGR2GRAY)
+
+        #Detectar persona
+
+        # Cargamos las librerías
+        people_cascade = cv2.CascadeClassifier('clasificadores/haarcascade_fullbody.xml')
+        people_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_fullbody.xml')
+
+        # Detecta las personas de la imagen en escala de grises
+        personas = people_cascade.detectMultiScale(img_gray, 1.1, 2)
+
+        # Por cada persona detectada se dibuja un rectángulo a su al rededor
+        npersonas = 0
+        for (x,y,w,h) in personas:
+            #cv2.rectangle(cv_image,(x,y),(x+w,y+h),(255,0,0),2)
+            roi_color = cv_image[y:y+h, x:x+w]
+            npersonas=npersonas+1
+
+        #Termina detectar persona
 
 
         lower_green = np.array([36, 25, 25])
@@ -92,14 +108,13 @@ class Ros2OpenCVImageConverter(Node):
 
         color = (0,0,255)
 
-        rectangulo = cv2.rectangle(cv_image, (x1,y1), (x2,y2), color, 2)
-     
-        
-
-        cv2.imshow('Verde detectado',rectangulo)
-        
-
-        cv2.waitKey(1) #aprieta una tecla 
+        if(npersonas>0):
+            rectangulo = cv2.rectangle(cv_image, (x1,y1), (x2,y2), color, 2)
+            cv2.imshow('Verde detectado',rectangulo)
+            cv2.waitKey(1) #aprieta una tecla 
+            print("Se ha detectado una persona de mi equipo")
+        else:
+            print("No hay personas verdes")
 
 def main(args=None):
 
